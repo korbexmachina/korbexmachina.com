5e98c4b0960f2c4fb6eeb878addce1e0decad25a
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-03-07 18:36:31 -0800

    set up mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -10,22 +10,21 @@ const dirname = path.resolve(fileURLToPath(import.meta.url), '../')
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+
+	extensions: ['.svelte', '.md', '.svx'],
+    preprocess: [
+		vitePreprocess(),
+        mdsvex({
+            extensions: ['.md', '.svx'],
+        })
+    ],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	},
-
-	plugins: [
-		svelte({
-			// tell svelte to handle mdsvex files
-			extensions: [".svelte", ".svx"],
-			preprocess: mdsvex()
-		})
-	]
+	}
 };
 
 export default config;
