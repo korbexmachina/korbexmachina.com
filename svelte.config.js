dcbf0a39c66dd8f8f43fc106de6dc8cea1d439ad
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2023-03-08 03:50:29 -0800

    setting up for gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { mdsvex } from 'mdsvex';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const dirname = path.resolve(fileURLToPath(import.meta.url), '../')
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -23,7 +25,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+      	// default options are shown. On some platforms
+      	// these options are set automatically — see below
+      	pages: 'build',
+      	assets: 'build',
+      	fallback: null,
+      	precompress: false,
+      	strict: true
+    }),
+	paths: {
+      base: dev ? '' : '/korbexmachina.com',
+    }
 	}
 };
 
